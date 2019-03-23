30ab2bb15f39301cba43b7b2eefb139b8131be7d
Matthew B <mjtb87@yahoo.com>
2019-03-22 18:41:35 -0600

    final extension file project

---
+++ folder_search_final.py
@@ -0,0 +1,35 @@
+
+
+import os
+from datetime import datetime
+
+
+
+def getTextExtension():
+    folder_path = 'python_task'
+    file_list = os.listdir(folder_path)
+    extension_dict = {}
+    for i in file_list:
+        if '.txt' in i:
+            mod_time = os.stat(os.path.join(folder_path, i)).st_mtime
+            extension_dict[i] =  datetime.fromtimestamp(mod_time)  
+    
+    return extension_dict
+        
+        
+    
+print(getTextExtension())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
